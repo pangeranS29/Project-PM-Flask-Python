83a83d5a053bda6b5ae9bb0c57b7fba5505a23ba
Pangeran <pangeransilaen1417@gmail.com>
2024-12-01 15:53:44 +0700

    penambahan halaman klasifikasi dan tentang

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/klasifikasi")
+def klasifikasi():
+    return render_template("klasifikasi.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
